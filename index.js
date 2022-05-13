58b89944b46aef928af6f8c1a552925a91b60b85
helder ferrari <helderferrari2@gmail.com>
2022-05-12 23:28:20 -0300

    Fixed variables

--- index.js
+++ index.js
@@ -10,4 +10,6 @@ app.use(express.json());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
-app.listen(process.env.APP_PORT);
+app.listen(process.env.APP_PORT || 3000, () => {
+  console.log('Express server listening on port %d in %s mode', process.env.APP_PORT, process.env.APP_ENV);
+});
